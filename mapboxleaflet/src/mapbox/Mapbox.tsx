bb18f9b9d337e2a1d116d115fdb94c3a692af4ca
Rudi <rudolf.gumzej@biss.hr>
2022-07-12 11:43:31 +0200

    mapbox

--- mapboxleaflet/src/mapbox/Mapbox.tsx
+++ mapboxleaflet/src/mapbox/Mapbox.tsx
@@ -66,13 +66,37 @@ const Map = ReactMapboxGl({
 mapboxgl.accessToken = token;
 
 export const Mapbox: FC = () => {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    (map.current as any) = new mapboxgl.Map({
+      container: (mapContainer as any).current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [-0.09, 51.505],
+      zoom: zoom,
+    });
+  });
+
+  useEffect(() => {
+    if (!map.current) return; // wait for map to initialize
+    (map.current as any).on("move", () => {
+      setLng((map.current as any).getCenter().lng.toFixed(4));
+      setLat((map.current as any).getCenter().lat.toFixed(4));
+      setZoom((map.current as any).getZoom().toFixed(2));
+    });
+  });
   // let mapboxMap: mapboxgl.Map;
   // const [mapState, setMapState] = useState<mapboxgl.Map | null>(null);
   // // debugger;
   // // mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN!;
 
-  // const geojson: GeoJSON.FeatureCollection<GeoJSON.Geometry> =
-  //   initialCircleCollection;
+  const geojson: GeoJSON.FeatureCollection<GeoJSON.Geometry> =
+    initialCircleCollection;
 
   // const [thetas, setThetas] = useState<number[]>([]);
 
@@ -101,85 +125,60 @@ export const Mapbox: FC = () => {
   // //   }
   // // }, []);
 
-  // const circlesUpdater = setInterval(() => {
-  //   if (mapState) {
-  //     debugger;
+  const circlesUpdater = setInterval(() => {
+    if (mapState) {
+      debugger;
 
-  //     for (let i = 0; i < thetas.length; i++) {
-  //       let ll =
-  //         (geojson.features[0].geometry as any).coordinates ??
-  //         initialCoordinates;
-  //       let theta = thetas[i];
+      for (let i = 0; i < thetas.length; i++) {
+        let ll =
+          (geojson.features[0].geometry as any).coordinates ??
+          initialCoordinates;
+        let theta = thetas[i];
 
-  //       const radEarth = 6378; // km
-  //       const dy = 0.01 * theta;
-  //       const dx = 0.01 * theta;
-  //       const degFactor = theta * 180;
-  //       const newLng: number = ll[0] + (dy / radEarth) * (degFactor / Math.PI);
-  //       const newLat: number =
-  //         ll[1] +
-  //         ((dx / radEarth) * (degFactor / Math.PI)) /
-  //           Math.cos((ll[0] * Math.PI) / degFactor);
+        const radEarth = 6378; // km
+        const dy = 0.01 * theta;
+        const dx = 0.01 * theta;
+        const degFactor = theta * 180;
+        const newLng: number = ll[0] + (dy / radEarth) * (degFactor / Math.PI);
+        const newLat: number =
+          ll[1] +
+          ((dx / radEarth) * (degFactor / Math.PI)) /
+            Math.cos((ll[0] * Math.PI) / degFactor);
 
-  //       const newPos = [newLat, newLng];
-  //       // debugger;
-  //       if (mapState.getBounds().contains(newPos as LngLatLike)) {
-  //         (geojson.features[0].geometry as any).coordinates = newPos;
-  //       } else {
-  //         (geojson.features[0].geometry as any).coordinates = getRandomLatLng();
-  //       }
-  //     }
+        const newPos = [newLat, newLng];
+        // debugger;
+        if (mapState.getBounds().contains(newPos as LngLatLike)) {
+          (geojson.features[0].geometry as any).coordinates = newPos;
+        } else {
+          (geojson.features[0].geometry as any).coordinates = getRandomLatLng();
+        }
+      }
 
-  //     if (thetas.length > 2000) {
-  //       clearInterval(circlesUpdater);
-  //     }
+      if (thetas.length > 2000) {
+        clearInterval(circlesUpdater);
+      }
 
-  //     // Add another circle with random direction
-  //     thetas.push(Math.random());
-  //     const newFeature = {
-  //       type: "Feature",
-  //       geometry: {
-  //         type: "Point",
-  //         coordinates: getRandomLatLng(),
-  //       },
-  //       properties: {},
-  //     };
-  //     // (geojson.features[0].geometry as any).coordinates.push(getRandomLatLng());
-  //     geojson.features.push(newFeature as any);
+      // Add another circle with random direction
+      thetas.push(Math.random());
+      const newFeature = {
+        type: "Feature",
+        geometry: {
+          type: "Point",
+          coordinates: getRandomLatLng(),
+        },
+        properties: {},
+      };
+      // (geojson.features[0].geometry as any).coordinates.push(getRandomLatLng());
+      geojson.features.push(newFeature as any);
 
-  //     // Update the map
-  //     // mapboxMap.getSource("circlesSet").setData(refData);
-  //     const source: mapboxgl.GeoJSONSource = mapState.getSource(
-  //       "circlesSet"
-  //     ) as mapboxgl.GeoJSONSource;
-  //     source.setData(geojson);
-  //   }
-  // }, 5);
-
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng, setLng] = useState(-70.9);
-  const [lat, setLat] = useState(42.35);
-  const [zoom, setZoom] = useState(9);
-
-  useEffect(() => {
-    if (map.current) return; // initialize map only once
-    (map.current as any) = new mapboxgl.Map({
-      container: (mapContainer as any).current,
-      style: "mapbox://styles/mapbox/streets-v11",
-      center: [-0.09, 51.505],
-      zoom: zoom,
-    });
-  });
-
-  useEffect(() => {
-    if (!map.current) return; // wait for map to initialize
-    (map.current as any).on("move", () => {
-      setLng((map.current as any).getCenter().lng.toFixed(4));
-      setLat((map.current as any).getCenter().lat.toFixed(4));
-      setZoom((map.current as any).getZoom().toFixed(2));
-    });
-  });
+      // Update the map
+      // mapboxMap.getSource("circlesSet").setData(refData);
+      const source: mapboxgl.GeoJSONSource = mapState.getSource(
+        "circlesSet"
+      ) as mapboxgl.GeoJSONSource;
+      source.setData(geojson);
+    }
+  }, 5);
 
   return (
     <div className="mapbox-container" id="map">
@@ -205,7 +204,11 @@ export const Mapbox: FC = () => {
           <Layer {...initialLayer} />
         </Source>
       </Map> */}
-      <div ref={mapContainer} className="map-container" />
+      <div ref={mapContainer} className="map-container">
+        <Source id="circlesSet" type="geojson" data={geojson}>
+          <Layer {...initialLayer} />
+        </Source>
+      </div>
     </div>
   );
 };
