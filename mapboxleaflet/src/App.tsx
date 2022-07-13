ffe25c4ff2e1e803cd3d612c7c93664c597914c6
Rudi <rudolf.gumzej@biss.hr>
2022-07-13 13:07:56 +0200

    added counter

--- mapboxleaflet/src/App.tsx
+++ mapboxleaflet/src/App.tsx
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { LeafLet } from "./leafLet/LeafLet";
 import { Mapbox } from "./mapbox/Mapbox";
-import { count } from "console";
 
 function App() {
   const [mapSwitcher, setMapSwitcher] = useState("");
   const [leafLetDots, setLeafLetDots] = useState(0);
   const [mapBoxDots, setMapBoxDots] = useState(0);
+  const [leafletCounter, setLeafletCounter] = useState(0);
+  const [mapboxCounter, setMapboxCounter] = useState(0);
+
+  const [startLeafletCounter, setStartLeafletCounter] = useState(false);
+  const [startMapboxCounter, setStartMapboxCounter] = useState(false);
 
   const setLeaflet = () => {
     setMapSwitcher("Leaflet");
@@ -19,18 +22,33 @@ function App() {
   };
   const countLeaflet = (dots: number) => {
     setLeafLetDots(dots);
+    setStartLeafletCounter(true);
   };
   const countMapbox = (dots: number) => {
     setMapBoxDots(dots);
+    setStartMapboxCounter(true);
   };
+
+  React.useEffect(() => {
+    if (startLeafletCounter && leafLetDots < 2000) {
+      setTimeout(() => setLeafletCounter(leafletCounter + 1), 1000);
+    }
+  }, [leafletCounter, startLeafletCounter]);
+
+  React.useEffect(() => {
+    if (startMapboxCounter && mapBoxDots < 2000) {
+      setTimeout(() => setMapboxCounter(mapboxCounter + 1), 1000);
+    }
+  }, [mapboxCounter, startMapboxCounter]);
+
   return (
     <div className="App">
       <div className="navbar">
         <button className="leaflet-button" onClick={setLeaflet}>
-          Leaflet: {leafLetDots}
+          Leaflet: {leafLetDots} - Time: {leafletCounter} s
         </button>
         <button className="mapbox-button" onClick={setMapbox}>
-          Mapbox: {mapBoxDots}
+          Mapbox: {mapBoxDots} - Time: {mapboxCounter} s
         </button>
       </div>
 
